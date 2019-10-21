51bdee77929498722d35d3b50a85fc70f7309fd4
Sudarmin Ahmad <55875766+Sudarmin-Ahmad@users.noreply.github.com>
2019-10-22 04:27:05 +0700

    TUGAS_3

---
+++ pgame-16049/code_tugas3.py
@@ -0,0 +1,12 @@
+# Ini Tugas 3. Praktik Git untuk MK Pemrograman Game
+# juga untuk latihan git push dan pull from terminal
+
+# Latihan Buat Variabel : 
+nama = "Nama Saya 	: Sudarmin A. Ahmad" 
+print (nama)
+npm = "NPM 		: 121055520116049" 
+print (npm)
+semester = "Semester 	: 7 (Tujuh)" 
+print (semester)
+matakuliah = "Mata Kuliah 	: Pemograman Game" 
+print (matakuliah)
