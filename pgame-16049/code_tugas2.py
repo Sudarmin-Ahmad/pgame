a85e30f10f300cd023360cf471b906e0eda8da8b
Sudarmin Ahmad <55875766+Sudarmin-Ahmad@users.noreply.github.com>
2019-10-22 04:25:52 +0700

    TUGAS_2

---
+++ pgame-16049/code_tugas2.py
@@ -0,0 +1,7 @@
+# Ini Tugas 2. Praktik Git untuk MK Pemrograman Game
+
+print ("Hello, my name is Sudarmin Ahmad") #Halo, nama saya Sudarmin Ahmad
+print ("I enjoyed studying at your ummu") #Saya senang kuliah di ummu
+#baris ini tidak akan dieksekusi
+print ("Halo Anak Informatika...") #baris ini dieksekusi
+#Baris ini juga merupakan Baris Komentar 
