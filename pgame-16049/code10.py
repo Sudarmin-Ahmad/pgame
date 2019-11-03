208f6b16d5ba5f53548afd6c91960a1007d15ca3
Sudarmin Ahmad <55875766+Sudarmin-Ahmad@users.noreply.github.com>
2019-11-03 08:03:24 +0700

    LATIHAN : Modul_Pgame_Operator_prak_3

--- pgame-16049/code10.py
+++ pgame-16049/code10.py
@@ -3,8 +3,8 @@
 a,b = 20,20
 a is b
 a is not b
-aku,dan,kamu="irja", "Love", "dona"
+aku,dan,kamu="irjaman", "Love", "dona"
 aku,dan,kamu
 aku is "kamu"
 aku is "dona"
-aku is "irja"
+aku is "irjaman"
