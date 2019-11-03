d2c2d42eee7e89077f47bd37d6b0830525ddef50
Sudarmin Ahmad <55875766+Sudarmin-Ahmad@users.noreply.github.com>
2019-11-03 07:43:49 +0700

    LATIHAN : Modul_Pgame_Operator_prak_3

---
+++ pgame-16049/code10.py
@@ -0,0 +1,11 @@
+#Operator Identitas
+
+ab = ['Dua Puluh, Dua Puluh']
+print (ab)
+a is b
+a is not b
+aku,dan,kamu="irja", "Love", "dona"
+aku,dan,kamu
+aku is "kamu"
+aku is "dona"
+aku is "irja"
---
+++ pgame-16049/code8.py
@@ -0,0 +1,25 @@
+#Operator Penugasan
+
+b = 10 	# Buat variabel b dan isi dengan nilai 10
+print (b)	
+
+a += 5	# Tambahkan nilai 5 ke variabel a
+print (a)
+
+a -= 10
+print (a)
+
+a *= 20
+print (a)
+
+a /= 60
+print (a)
+
+a %= 40
+print (a)
+
+a **= 20
+print (a)
+
+b //= 30
+print (b)
